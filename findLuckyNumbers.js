e87b59d0d829ce53e4db3a3e6d08527eb68083e0
Greg Stickel <gregory.stickel@live.com>
2022-09-20 08:48:50 -0400

    Revision 1

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,1 +1,23 @@
 // Write your code below this line.
+
+let n = 10; //n equals number of unique numbers to generate
+
+// generate random unique number
+function randomNum(range, outputCount) {
+  let arr = [];
+  for (let i = 1; i <= range; i++) {
+    arr.push(i);
+  }
+
+  let result = [];
+
+  for (let i = 1; i <= outputCount; i++) {
+    const random = Math.floor(Math.random() * (range - i));
+    result.push(arr[random]);
+    arr[random] = arr[range - i];
+  }
+
+  return result;
+}
+
+console.log(randomNum(10, n));
